89d81a81a091c46d570ea4b986bdb18acad3b53a
Martin Povolny <mpovolny@redhat.com>
2018-07-09 15:57:44 +0200

    Add React component name.

--- app/toolbar_overrides/ems_cloud_center.rb
+++ app/toolbar_overrides/ems_cloud_center.rb
@@ -10,9 +10,11 @@ module ManageIQ
               t = N_('Magic'),
               t,
               :data  => {'function'      => 'sendDataWithRx',
-                         'function-data' => {:controller => 'provider_dialogs', # this one is required
-                                             :button     => :magic,
-                                             :ems_id     => EmsCloud.first.id}.to_json},
+                         'function-data' => {:controller     => 'provider_dialogs', # this one is required
+                                             :button         => :magic,
+                                             :modal_title    => N_('Create a Security Group'),
+                                             :component_name => 'CreateAmazonSecurityGroupForm',
+                                             :ems_id         => EmsCloud.first.id}.to_json}, # this line to be removed, usage replaced with ManageIQ.record.recordId
               :klass => ApplicationHelper::Button::ButtonWithoutRbacCheck),
             button(
               :magic_dialog,
